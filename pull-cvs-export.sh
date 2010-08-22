418e60436148f84eea4b567d4d37d86fbbcc5513
Klaus Purer <klaus.purer@gmail.com>
2010-08-22 15:38:21 +0200

    fixed comment typo

--- pull-cvs-export.sh
+++ pull-cvs-export.sh
@@ -36,7 +36,7 @@ for COMMIT in $NEW_COMMITS
 do
   echo '** Exporting commit to CVS: **'
   git cvsexportcommit -ucpw $CVS_DIR $COMMIT || clean_up
-  # save succeddful exported commit to file
+  # save successful exported commit to file
   echo $COMMIT > $LAST_EXPORT_FILE
 done
 
